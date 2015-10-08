3f9ae1229b7a97865b3ce32d48bc831687339e3d
rodickmini <rodickcai@gmail.com>
2015-10-08 17:08:28 +0800

    修改了gulpfile工作流业务逻辑

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
-var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+var rename = require('gulp-rename');
 
+gulp.task('cleanup', function() {
+    return gulp.src('dist', {read: false})
+        .pipe(clean({force: true}));
+});
+
+gulp.task('js', ['cleanup'], function () {
+    return gulp.src('jquery-iconbox.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('default', function() {
-    return gulp.src('jquery-iconbox.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+    gulp.run('js');
+    gulp.watch('jquery-iconbox.js', ['js']);
 });
